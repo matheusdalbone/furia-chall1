bcc917663f7d3e82c24202b49baec650f74854ea
matheusdalbone <matheusdalbone7@gmail.com>
2025-04-28 18:34:58 -0300

    refactor: chat messages and background

--- frontend/furia-chatbot/src/components/chat/index.jsx
+++ frontend/furia-chatbot/src/components/chat/index.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./style.module.css";
 import { IoSendSharp } from "react-icons/io5";
 import { assistantResponse } from "../../services/api";
+import Markdown from 'react-markdown';
 
 const Chat = () => {
   const [userQuestion, setUserQuestion] = useState("");
   const [messages, setMessages] = useState([]);
 
+  const scrollInto = useRef();
+
   async function postAssistantResponse(question) {
     assistantResponse(question)
       .then((response) => {
         setMessages((messages) => [...messages, response.data]);
-        console.log(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -33,16 +35,28 @@ const Chat = () => {
     setUserQuestion("");
   };
 
+  useEffect(() => {
+    if (!messages.length <= 0) {
+      scrollInto.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
   return (
     <>
       <section className={styles.chatSection}>
         <div className={styles.chatDisplay}>
           <div className={styles.chatMessages}>
-            <div>
-              {messages.map((message) =>
-                message.type == "user" ? <div className={styles.userMessage}>User: {message.text}</div> : <div className={ styles.assistantMessage }>Assistant: { message.text}</div>
-              )}
-            </div>
+            {messages.map((message) =>
+              message.type == "user" ? (
+                <div ref={scrollInto} className={styles.userMessage}>
+                  <span>{message.text}</span>
+                </div>
+              ) : (
+                <div className={styles.assistantMessage}>
+                    <Markdown>{message.text}</Markdown>
+                </div>
+              )
+            )}
           </div>
           <div className={styles.inputSection}>
             <form onSubmit={handleSubmit}>
