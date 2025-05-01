d8e58ed6e1d47a43ce652217ba91f17f3d12caa4
matheusdalbone <matheusdalbone7@gmail.com>
2025-05-01 14:53:39 -0300

    build: frontend deploy

--- frontend/furia-chatbot/src/services/api.js
+++ frontend/furia-chatbot/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiUrl = axios.create({
-  baseURL: 'http://127.0.0.1:5000/'
+  baseURL: 'https://furia-chatbot-tau.vercel.app/'
 })
 
 export const assistantResponse = async (question) => {
